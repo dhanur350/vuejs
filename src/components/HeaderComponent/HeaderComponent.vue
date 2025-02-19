818bb655191538d909f38d1296b570ab01c675c5
dhanur350 <dhanur297@gmail.com>
2025-02-19 18:09:43 +0530

    useState into ref

--- src/components/HeaderComponent/HeaderComponent.vue
+++ src/components/HeaderComponent/HeaderComponent.vue
@@ -1,9 +1,29 @@
 <script lang="ts">
-import headerComponent from './headerComponent';
-export default headerComponent;
+import { ref, defineComponent } from 'vue';
+
+export default defineComponent ({
+  name: "HeaderComponent",
+  props: {
+    message: String,
+  },
+  setup(props, ctx) {
+
+    const refCount = ref(true);
+
+    const handleclick = (event: MouseEvent) => {
+      refCount.value = !refCount.value;
+      console.log("header", event);
+    };
+
+    return { refCount, handleclick};
+  },
+});
 </script>
 
 <template>
   <header>{{ message }}</header>
+  <span>{{ refCount }}</span>
   <button @click="handleclick">Header Button</button>
 </template>
+
+
